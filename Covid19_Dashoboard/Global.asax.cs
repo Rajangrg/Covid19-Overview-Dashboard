4315434fd51c839ae49fbcf7e17708e32642ae8a
Rajan G <rajangrg1@hotmail.com>
2020-04-28 14:44:02 +1200

    UI - new theme and design

--- Covid19_Dashoboard/Global.asax.cs
+++ Covid19_Dashoboard/Global.asax.cs
@@ -15,7 +15,6 @@ namespace Covid19_Dashoboard
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
